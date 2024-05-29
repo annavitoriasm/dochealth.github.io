b84d8bd52289d7a1dc8325981ee5f42039d3a441
Anna Vitoria <161900970+lilabacana@users.noreply.github.com>
2024-05-29 20:46:57 -0300

    js lista de sintomas

--- src/script/selectSint.js
+++ src/script/selectSint.js
@@ -1,26 +1,40 @@
 // '---SELECIONAR OS SINTOMAS---'
 
 
-// const principal = document.querySelector('.selecionados');
+const principal = document.querySelector('.selecionados');
 
-// function selecionar(sintoma) {
-//     let myDiv = document.createElement('div');
-//     myDiv.innerHTML = sintoma;
-//     myDiv.classList.add('opcoes');
-//     principal.appendChild(myDiv);
-// }
-
-const opcoesSintomas = document.querySelectorAll('.opcoesSintomas');
-let sintomas = ['Febre', 'Dor de cabeça'];
-
-sintomas.map((novosintoma, index) => {
-
+function selecionar(sintoma) {
     let myDiv = document.createElement('div');
-    myDiv.innerHTML = novosintoma;
+    myDiv.innerHTML = sintoma;
     myDiv.classList.add('opcoes');
-    opcoesSintomas.appendChild(myDiv);
+    principal.appendChild(myDiv);
 
-})
+    myDiv.addEventListener('click', function () {
+        console.log('teste');
+    })
+}
+
+let opcoesSintomas = document.getElementById('listagem');
+let sintomas = ['Febre', 'Dor de cabeça', 'Tosse', 'Gripe'];
+
+window.onload = function () {
+    sintomas.map((novosintoma) => {
+
+        let myDiv = document.createElement('div');
+
+        myDiv.innerHTML = novosintoma;
+        myDiv.classList.add('opcoes');
+        opcoesSintomas.appendChild(myDiv);
+
+        myDiv.addEventListener('click', function () {
+            selecionar(novosintoma);
+        })
+    })
+}
+
+
+
+
 
 
 

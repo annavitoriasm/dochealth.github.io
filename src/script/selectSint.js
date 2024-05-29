c472a7ee0b9be0114f2c4b240e027ce7f3cdb796
Anna Vitoria <161900970+lilabacana@users.noreply.github.com>
2024-05-29 19:34:43 -0300

    js

--- src/script/selectSint.js
+++ src/script/selectSint.js
@@ -1,17 +1,26 @@
 // '---SELECIONAR OS SINTOMAS---'
-const principal = document.querySelector('.selecionados');
-
-function selecionar(sintoma) {
-    let myDiv = document.createElement('div');
-    myDiv.innerHTML = sintoma;
-    myDiv.classList.add('opcoes');
-    principal.appendChild(myDiv);
-}
 
 
+// const principal = document.querySelector('.selecionados');
+
+// function selecionar(sintoma) {
+//     let myDiv = document.createElement('div');
+//     myDiv.innerHTML = sintoma;
+//     myDiv.classList.add('opcoes');
+//     principal.appendChild(myDiv);
+// }
+
+const opcoesSintomas = document.querySelectorAll('.opcoesSintomas');
 let sintomas = ['Febre', 'Dor de cabeça'];
 
-sintomas.map((novosintoma, index) => )
+sintomas.map((novosintoma, index) => {
+
+    let myDiv = document.createElement('div');
+    myDiv.innerHTML = novosintoma;
+    myDiv.classList.add('opcoes');
+    opcoesSintomas.appendChild(myDiv);
+
+})
 
 
 
